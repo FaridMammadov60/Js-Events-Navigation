6eaa7790a5a89fa06d09e4e8544187931d3ca8be
FaridMammadov60 <faridmma@code.edu.az>
2022-05-25 08:05:46 -0700

    car sound add

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -2,6 +2,7 @@
 let box = document.getElementById("icon");
 let marginleft = 0;
 let margintop = 0;
+var audio = document.getElementById('audio');
 
 document.addEventListener('keydown', function (e) {
     switch (e.keyCode) {
@@ -10,36 +11,39 @@ document.addEventListener('keydown', function (e) {
             marginleft = marginleft - 100;
             box.style.transform = "rotateZ(-180deg)";
             box.style.marginLeft = `${marginleft}px`;
-
+            audio.play();
             break;
         case 38:
             box.style.color = "blue";
             margintop = margintop - 100;
             box.style.transform = "rotate(270deg)";
             box.style.marginTop = `${margintop}px`;
+            audio.play();
             break;
         case 39:
             box.style.color = "green";
             marginleft = marginleft + 100;
             box.style.transform = "rotate(0deg)";
             box.style.marginLeft = `${marginleft}px`;
+            audio.play();
             break;
         case 40:
             box.style.color = "pink";
             margintop = margintop + 100;
             box.style.transform = "rotate(90deg)";
             box.style.marginTop = `${margintop}px`;
+            audio.play();
             break;
     }
 });
 //Side bar start
-let sidebar=document.getElementById("sidebar");
-let closebtn=document.getElementById("closebtn");
-let openbtn=document.getElementById("openbtn");
-
-openbtn.addEventListener("click", function(){
-    sidebar.classList.remove("close");
-})
-closebtn.addEventListener("click", function(){
-    sidebar.classList.add("close");
-})
+//let sidebar=document.getElementById("sidebar");
+//let closebtn=document.getElementById("closebtn");
+//let openbtn=document.getElementById("openbtn");
+//
+//openbtn.addEventListener("click", function(){
+//    sidebar.classList.remove("close");
+//})
+//closebtn.addEventListener("click", function(){
+//    sidebar.classList.add("close");
+//})
